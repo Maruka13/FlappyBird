23ca568f7782ff23962cb513670608e77b820600
Maruka13 <emanuelle_carollina@hotmail.com>
2026-03-09 15:44:28 -0300

    feat: create Bird class and initialize physics properties

--- FlappyBird.py
+++ FlappyBird.py
@@ -16,3 +16,26 @@ IMAGENS_PASSARO = [
 
 pygame.font.init()
 FONTE_PONTOS = pygame.font.SysFont("arial", 50)
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+    # animações da rotação / caindo em parábola
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.velocidade = 0
+        self.angulo = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0  # qual img está usando no momento
+        self.imagem = IMGS[0]
+
+class Cano:
+    pass
+
+class Chao:
+    pass
