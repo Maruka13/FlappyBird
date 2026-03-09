2e87e281c6105348d50f6fe3f3e618422345ba3a
Maruka13 <emanuelle_carollina@hotmail.com>
2026-03-09 15:09:12 -0300

    feat: define game constants and scale initial assets

---
+++ FlappyBird.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import random
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x()
+IMAGEM_CHAO = pygame.transform.scale2x()
+IMAGEM_BACKGROUND = pygame.transform.scale2x()
+IMAGENS_PASSARO = pygame.transform.scale2x()
