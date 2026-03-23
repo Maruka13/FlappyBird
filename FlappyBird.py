21fd77ac669e861708d2cc38f2722f8784b5a7e4
Maruka13 <emanuelle_carollina@hotmail.com>
2026-03-23 09:36:47 -0300

    feat: implement pixel-perfect collision logic for Pipe class

--- FlappyBird.py
+++ FlappyBird.py
@@ -133,6 +133,25 @@ class Cano:
         tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
         tela.blit(self.CANO_BASE, (self.x, self.pos_base))
 
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        # cria as masks para os canos sup e infr com base nas imgs
+        topo_mask = pygame.mask.from_surface(passaro.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(passaro.CANO_BASE)
+        # calcula a dist (offset) entre o pássaro e o cano do topo e da base
+        distancia_topo = (self.x - passaro.x, self.pos_topo - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.pos_base - round(passaro.y))
+        # verif se existe ponto de sobreposição entre as masks
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        # retorna Verdadeiro se houver colisão em qualquer um dos canos
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
+
+
 
 class Chao:
     pass
